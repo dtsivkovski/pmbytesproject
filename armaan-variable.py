d305202e55fa8aedbc10f7374f5fc28216d849b9
xXAASXx <89277945+xXAASXx@users.noreply.github.com>
2021-09-22 12:55:24 -0700

    Armaan Variable DONE!

--- armaan-variable.py
+++ armaan-variable.py
@@ -1,5 +1,6 @@
 # Challenge 1: Name the variable types of the following variables. Print them out into console in the format "Variable: Variable Type" (might have to google "how to print variables in python")
-print('challenge 1')
+
+print('--------Challenge 1!--------')
 var1 = 3
 var2 = "Mr. Mortensen"
 var3 = 'f'
@@ -9,19 +10,15 @@ print(var2, 'is a string')
 print(var3, 'is a character')
 print(var4, 'is a float')
 
-# Challenge 2: Pass list1 into list2. However, list2 must contain the elements of list1 in order. Print list2. +0.3 if you can create a function to order a list and can display it on your website
-print('Challenge 2')
+print(' ')
+print("--------Challenge 2!--------")
 list1 = [5, 3, 4, 1, 2]
-# list2 = ?
-# def orderList (list):
-#  ?
 list2 = [1, 2, 3, 4, 5]
 print(list1, 'is now', list2)
-# Challenge 3: Find a way to add 3 to each element in the array. Then, take the average of the array and put it into the variable avg. +0.2 if you can turn this into a function and display it on your website.
-print('Challenge 3')
+print(' ')
+print('--------Challenge 3!--------')
+
 averageList = [23, 41, 90, 55, 71, 83]
-avg= ((averageList[0]) + (averageList[1]) + (averageList[3]) + (averageList[4]) + (averageList[5]) /6
-      print(averageList, 'average is ', avg)
-# avg = ?
-# def averageList (list):
-#  ?
+avg= ((averageList[0] + 3) + (averageList[1] + 3) + (averageList[2] +3) +
+      (averageList[3] + 3) + (averageList[4] + 3) + (averageList[5]+ 3)) / 6
+print('The average of ', averageList, 'is ', avg)
