e3327ce00a37f597c0f4fb410141c91215166339
Chom642 <awesomejedi115@gmail.com>
2021-10-12 13:23:12 -0700

    Week 7 hackathon

--- HackathonTests/ChrisWeek7.py
+++ HackathonTests/ChrisWeek7.py
@@ -1,4 +1,4 @@
-#Activity 1
+#Challenge 1
 
 student_list = ['pam', 'rob', 'joe', 'greg', 'bob', 'amy', 'matt']
 print(student_list[2:5])
@@ -10,7 +10,7 @@ if 'rob' in student_list:
 else:
     print('No, Rob is not on the student list')
 
-#Activity 2
+#Challenge 2
 
 p1 = { "name":"John", "age":61, "city":"Eugene"}
 p2 = { "name":"Risa", "age":16, "city":"New York"}
@@ -20,24 +20,24 @@ p4 = { "name":"Shekar", "age":16, "city":"San Francisco"}
 list_of_people = [p1, p2, p3, p4]
 # write some code to Print List of people one by one
 
-for i in dict_people['people']:
-    print(i['name'])
+#for i in dict_people['people']:
+#    print(i['name'])
 
 
 print(" ")
 
 # Creating a json dump
 print(" - Dumps - Python to JSON String - ")
-json_people = json.dumps(list_of_people)
-print(type(json_people))
-json_dict = json.loads(json_people)
+#json_people = json.dumps(list_of_people)
+#print(type(json_people))
+#json_dict = json.loads(json_people)
 # to list
-names = [person['name'] for person in json_dict]
-print("Names of people to list: " + str(names))
+#names = [person['name'] for person in json_dict]
+#print("Names of people to list: " + str(names))
 print(" ")
 print("Names of people: ")
 # pretty print Names of People (SEE BELOW)
-for name in names:
-    print(name)
+#for name in names:
+#    print(name)
 
 
