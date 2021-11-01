e4e556f5b71d6a27852629f8fcb8ff09872b34a7
Daniel Tsivkovski <dtsivkovs@gmail.com>
2021-11-01 12:37:05 -0700

    Changes to api and animalgallery

--- algorithm/bgimglist.py
+++ algorithm/bgimglist.py
@@ -2,6 +2,7 @@
 bgimg_list = [
     "lizard1.jpg", "jungle.jpg", "siamang.jpg",
     "butterfly1.jpg", "forest1.jpg", "hummingbird.jpg",
-    "kingfisher.jpg", "redfox.jpg",
+    "kingfisher.jpg", "redfox.jpg", "leopard.jpg",
+    "squirrel.jpg"
 ]
 
