0a3f3c07bc61f6294159b5db72a180f837900242
Daniel Tsivkovski <dtsivkovs@gmail.com>
2021-11-02 14:04:35 -0700

    update to backgrounds and home page

--- algorithm/bgimglist.py
+++ algorithm/bgimglist.py
@@ -1,7 +1,8 @@
 
 bgimg_list = [
     "lizard1.jpg", "jungle.jpg", "siamang.jpg",
-    "butterfly1.jpg", "forest1.jpg", "hummingbird.jpg",
+    "butterfly1.jpg", "forest.jpg", "forest1.jpg",
+    "forest2.jpg", "hummingbird.jpg",
     "kingfisher.jpg", "redfox.jpg", "leopard.jpg",
     "squirrel.jpg"
 ]
